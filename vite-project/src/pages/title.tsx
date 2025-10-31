48a5cf39d16c53a8705f154b3e63cfa534ad2605
Lukuv <defaulttwich@gmail.com>
2025-10-31 12:38:32 +0100

    add nyt credits

--- vite-project/src/pages/title.tsx
+++ vite-project/src/pages/title.tsx
@@ -3,6 +3,7 @@ import "../main.sass"
 function Title() {
   return (
     <div className="titleContainer">
+      <img src="/nyt_credits.png" alt="Data provided by The New York Times" />
       <img src="/newsify_logo.svg" alt="" />
       <h1>Newsify</h1>
     </div>
